f24e846ed92e4814739d01992c05ddc1434994f1
kstrauch <kstrauch@groix.staff.cs.uni-potsdam.de>
2021-09-02 16:00:11 +0200

    test stuff

--- otfgrounding/propagator/propagator.py
+++ otfgrounding/propagator/propagator.py
@@ -1,3 +1,5 @@
+from clingo import parse_term
+
 from typing import Dict, List, Any, Set
 from collections import defaultdict
 
@@ -10,19 +12,62 @@ split_cons_re = r",\s+(?=[^()]*(?:\(|$))"
 # split on comma except between ""
 atom_name_re = r"(?P<name>\w+)\("
 atom_params_re = r"(\w+)[,\)]"
-class Propagator:
 
-	__slots__ = []
+
+class Atom:
+
+	def __init__(self, name, variables):
+		self.name = name
+		self.variables = variables
+		self.arity = len(variables)
+
+	def __str__(self):
+		return f"{self.name}{*self.variables,}".replace("'","")
+
+class DomConstraint:
+
+	def __init__(self, dom_c):
+		self.dom_c = dom_c
+
+	def test(self, variables):
+		expression = self.dom_c
+
+		for var,val in variables.items():
+			print(var, val)
+			expression = expression.replace(var,str(val))
+			print(expression)
+
+		print(expression)
+		print(parse_term(expression))
+
+class Propagator:
 
 	def __init__(self, line):
 		pre_literals = re.split(split_cons_re, line.replace(":-","").strip()[:-1])
 		print(pre_literals)
+		
+		self.atoms = []
+		
+		self.all_vars = set()
+
 		for atom in pre_literals:
+			if any(c in ["=", ">", "<"] for c in atom):
+				dc = DomConstraint(atom)
+				dc.test({"D": 2, "D2": 1})
+
 			s = re.search(atom_name_re, atom)
-			print(s)
-			print(s.group("name"))
-			s = re.findall(atom_params_re, atom)
-			print(s)
+			name = s.group("name")
+
+			variables = re.findall(atom_params_re, atom)
+			
+			atom = Atom(name, variables)
+
+			print(atom)
+			
+			self.atoms.append(atom)
+			self.all_vars.update(atom.variables)
+
+		print(self.all_vars)
 
 
 	@util.Timer("Prop_init")
